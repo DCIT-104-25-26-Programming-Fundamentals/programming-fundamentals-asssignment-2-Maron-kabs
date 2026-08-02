d77a39c0dd405f14a0727701efb0cee1047874f4
Maron-kabs <maronkabukuor1954@gmail.com>
2026-08-02 16:34:01 +0000

    Complete Fibonacci sequence generator assignment

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,78 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+// Function to print first N Fibonacci terms
+void printFibonacci(int n)
+{
+    int first = 0;
+    int second = 1;
+
+    cout << "Fibonacci sequence: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << first << " ";
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    cout << endl;
+}
+
+// Function to check if a number is a Fibonacci number
+bool isFibonacci(int number)
+{
+    int first = 0;
+    int second = 1;
+
+    while (first <= number)
+    {
+        if (first == number)
+        {
+            return true;
+        }
+
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int n;
+
+    cout << "How many terms? ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: Number of terms must be positive." << endl;
+        return 0;
+    }
+
+    printFibonacci(n);
+
+    int number;
+
+    cout << "Enter a number to check: ";
+    cin >> number;
+
+    if (isFibonacci(number))
+    {
+        cout << number << " is a Fibonacci number." << endl;
+    }
+    else
+    {
+        cout << number << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
